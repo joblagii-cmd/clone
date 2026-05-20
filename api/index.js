bb65e2869b22aa3d2731821607d0155919dd605e
joblagii-cmd <joblagii@gmail.com>
2026-05-20 15:10:56 +0530

    Update index.js

--- api/index.js
+++ api/index.js
@@ -1,5 +1,5 @@
 module.exports = async (req, res) => {
-  const shopifyDomain = "snagajob.com";
+  const shopifyDomain = "simplyhired.com";
   const proxyHost = req.headers.host;
 
   const targetURL = `https://${shopifyDomain}${req.url}`;
