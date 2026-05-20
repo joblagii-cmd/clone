ba8dc1dfd6f2a1adde9aee904e2bc4b89a0e1293
joblagii-cmd <joblagii@gmail.com>
2026-05-20 15:19:23 +0530

    Update index.js

--- api/index.js
+++ api/index.js
@@ -1,5 +1,5 @@
 module.exports = async (req, res) => {
-  const shopifyDomain = "jooble.org";
+  const shopifyDomain = " https://www.talent.com";
   const proxyHost = req.headers.host;
 
   const targetURL = `https://${shopifyDomain}${req.url}`;
